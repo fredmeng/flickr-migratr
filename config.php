6095a2e32edcc6d194f22204cc178123e0a4e064
Fred <fred@cyaontheroad.com>
2019-10-31 21:49:11 +1100

    config.php

---
+++ config.php
@@ -0,0 +1,21 @@
+<?php
+
+// source
+const src_api_key = '';
+const src_api_secret = '';
+const src_oauth_token = '';
+const src_oauth_token_secret = '';
+const src_user_id = '';
+
+// destination
+const dest_api_key = '';
+const dest_api_secret = '';
+const dest_oauth_token = '';
+const dest_oauth_token_secret = '';
+
+const api_endpoint = 'https://www.flickr.com/services/rest/';
+const oauth_version = '1.0';
+const oauth_signature_method = 'HMAC-SHA1';
+
+
+?>
